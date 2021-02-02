8902a8ad9f801834de5d992aebece2127c2f44e8
earthssu <entjq9502@khu.ac.kr>
2021-02-02 14:19:22 +0900

    es6 문법 변경

--- TodoList.js
+++ TodoList.js
@@ -1,25 +1,21 @@
-var button = document.getElementById("button");
-var input = document.getElementById("input");
-var list = document.getElementById("list-box");
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const list = document.getElementById("list-box");
 
-button.addEventListener("click", clickButton);
-
-todoId = 1;
-
-function clickButton() {
-  var todo = document.createElement("li");
+let todoId = 1;
+const clickButton = () => {
+  let todo = document.createElement("li");
   todo.setAttribute("class", "list-item");
   todo.setAttribute("id", "li" + todoId);
   todo.innerHTML = input.value;
-  todo.innerHTML +=
-    "<button type='button' id='remove' onClick='remove(" +
-    todoId +
-    ")'>X</button>";
+  todo.innerHTML += `<button type='button' id='remove' onClick='remove("${todoId}")'>X</button>`;
   list.appendChild(todo);
   todoId++;
-}
+};
 
-function remove(id) {
-  var li = document.getElementById("li" + id);
+const remove = (id) => {
+  let li = document.getElementById("li" + id);
   list.removeChild(li);
-}
+};
+
+button.addEventListener("click", clickButton);
