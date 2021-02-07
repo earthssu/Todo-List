70dfb5e744ff84583e8b1172552d5330a23e30f9
earthssu <entjq9502@khu.ac.kr>
2021-02-07 17:55:28 +0900

    마감 기한 입력 추가

--- README.md
+++ README.md
@@ -6,13 +6,22 @@
 
 ## demo
 
-![demo01](https://user-images.githubusercontent.com/39795055/107003350-fa4a5a00-67cf-11eb-8490-549f2eb82e44.png)
-![demo02](https://user-images.githubusercontent.com/39795055/107003354-fb7b8700-67cf-11eb-856c-098705cdc5da.png)
+![demo01](https://user-images.githubusercontent.com/39795055/107141543-ac1e8d80-696c-11eb-80b4-3cc4ea5d200b.png)
+![demo02](https://user-images.githubusercontent.com/39795055/107141544-ad4fba80-696c-11eb-8824-7e6b7979060d.png)
 
-### 기능(2020.02.05 업데이트)
+### 기능(2020.02.07 업데이트)
 
 - 할 일 추가 : 입력 박스에 할 일을 작성하고 오른쪽 '+' 버튼을 누른다
 - 할 일 삭제 : 할 일 목록 각각 오른쪽 '삭제' 버튼을 누른다
 - 완료 : 완료된 항목은 오른쪽 '완료' 버튼을 눌러 '완료 목록'으로 이동시킨다. 완료 목록에서도 삭제가 가능하다.
+- 남은 일자 표시 : 할 일 입력시 마감 날짜를 함께 설정할 경우 마감 일정과 함께 남은 날짜가 표시된다.
+
+<br/>
+
+#### 개선 or 추가
+
+- 업로드시 엔터키에도 반응하도록 설정
+- 날짜순 정렬
+- 변수/함수 이름
 
 ---
--- TodoList.js
+++ TodoList.js
@@ -1,41 +1,54 @@
-const add_button = document.getElementById("add_button");
+const add_button = document.querySelector("#add_button");
 const complete_button = document.getElementById("complete");
-const input = document.getElementById("input");
+const todo_input = document.querySelector("#todo_input");
+const date_input = document.querySelector("#date_input");
 const todo_list = document.getElementById("todo_list");
 const complete_list = document.getElementById("complete_list");
 
 let todoId = 1;
-const ClickButton = () => {
+const clickButton = () => {
+  let today = new Date();
+  const dateSplit = date_input.value.split("-");
+  let dueDate = new Date(dateSplit[0], dateSplit[1] - 1, dateSplit[2]);
+  let gap = today.getTime() - dueDate.getTime();
+  let dday = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1;
+
   let todo = document.createElement("li");
   todo.setAttribute("class", "list_item");
   todo.setAttribute("id", "li" + todoId);
-  todo.innerHTML = input.value;
-  todo.innerHTML += `<button type='button' id='remove' onClick='TodoRemove("${todoId}")'>삭제</button>`;
-  todo.innerHTML += `<button type='button' id='complete'onClick='Complete("${todoId}")'>완료</button>`;
+  todo.innerHTML = `<span class='item'>${todo_input.value}</span>`;
+  todo.innerHTML += `<span class='item'>${date_input.value}</span>`;
+  todo.innerHTML += `<span class='dday'>D-${dday}</span>`;
+  todo.innerHTML += `<button type='button' id='remove' onClick='todoRemove("${todoId}")'>삭제</button>`;
+  todo.innerHTML += `<button type='button' id='complete'onClick='complete("${todoId}")'>완료</button>`;
   todo_list.appendChild(todo);
   todoId++;
 };
 
-const Complete = (id) => {
+const complete = (id) => {
   let li = document.getElementById("li" + id);
+  const content = li.querySelectorAll(".item");
+
   todo_list.removeChild(li);
 
   let complt = document.createElement("li");
   complt.setAttribute("class", "list_item");
   complt.setAttribute("id", "li" + id);
-  complt.innerHTML = li.innerText.split("삭제")[0];
-  complt.innerHTML += `<button type='button' id='remove' onClick='CompleteRemove("${id}")'>삭제</button>`;
+  complt.innerHTML = `<span class='item'>${content[0].innerHTML}</span>`;
+  complt.innerHTML += `<span class='item'>${content[1].innerHTML}</span>`;
+  complt.innerHTML += `<button type='button' id='remove' onClick='completeRemove("${id}")'>삭제</button>`;
+
   complete_list.appendChild(complt);
 };
 
-const TodoRemove = (id) => {
+const todoRemove = (id) => {
   let li = document.getElementById("li" + id);
   todo_list.removeChild(li);
 };
 
-const CompleteRemove = (id) => {
+const completeRemove = (id) => {
   let li = document.getElementById("li" + id);
   complete_list.removeChild(li);
 };
 
-add_button.addEventListener("click", ClickButton);
+add_button.addEventListener("click", clickButton);
