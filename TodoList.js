f8ee8dfea40862cdf54ad065798caf41cb65f7bd
earthssu <entjq9502@khu.ac.kr>
2021-02-09 21:16:03 +0900

    select 추가

--- TodoList.js
+++ TodoList.js
@@ -1,27 +1,30 @@
-const add_button = document.querySelector("#add_button");
-const complete_button = document.getElementById("complete");
-const todo_input = document.querySelector("#todo_input");
-const date_input = document.querySelector("#date_input");
-const todo_list = document.getElementById("todo_list");
-const complete_list = document.getElementById("complete_list");
+const addButton = document.querySelector("#add-button");
+const todoInput = document.querySelector("#todo-input");
+const dateInput = document.querySelector("#date-input");
+const selectInput = document.querySelector("#todo-select");
+const todoList = document.getElementById("todo-list");
+const completeList = document.getElementById("complete-list");
 
 let todoId = 1;
+
 const clickButton = () => {
   let today = new Date();
-  const dateSplit = date_input.value.split("-");
-  let dueDate = new Date(dateSplit[0], dateSplit[1] - 1, dateSplit[2]);
+  const [year, month, day] = dateInput.value.split("-");
+  let dueDate = new Date(year, month - 1, day);
   let gap = today.getTime() - dueDate.getTime();
   let dday = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1;
 
   let todo = document.createElement("li");
-  todo.setAttribute("class", "list_item");
+  todo.setAttribute("class", "list-item");
   todo.setAttribute("id", "li" + todoId);
-  todo.innerHTML = `<span class='item'>${todo_input.value}</span>`;
-  todo.innerHTML += `<span class='item'>${date_input.value}</span>`;
-  todo.innerHTML += `<span class='dday'>D-${dday}</span>`;
-  todo.innerHTML += `<button type='button' id='remove' onClick='todoRemove("${todoId}")'>삭제</button>`;
-  todo.innerHTML += `<button type='button' id='complete'onClick='complete("${todoId}")'>완료</button>`;
-  todo_list.appendChild(todo);
+  todo.innerHTML = `<span class='item'>${todoInput.value} 
+  <span class="select-item">(${selectInput.value})</span></span>
+  <span class='item'>${dateInput.value}</span>
+  <span class='dday'>D-${dday}</span>
+  <button type='button' class='remove' onClick='todoRemove("${todoId}")'>삭제</button>
+  <button type='button' class='complete'onClick='complete("${todoId}")'>완료</button>`;
+
+  todoList.appendChild(todo);
   todoId++;
 };
 
@@ -29,26 +32,26 @@ const complete = (id) => {
   let li = document.getElementById("li" + id);
   const content = li.querySelectorAll(".item");
 
-  todo_list.removeChild(li);
+  todoList.removeChild(li);
 
   let complt = document.createElement("li");
-  complt.setAttribute("class", "list_item");
+  complt.setAttribute("class", "list-item");
   complt.setAttribute("id", "li" + id);
-  complt.innerHTML = `<span class='item'>${content[0].innerHTML}</span>`;
-  complt.innerHTML += `<span class='item'>${content[1].innerHTML}</span>`;
-  complt.innerHTML += `<button type='button' id='remove' onClick='completeRemove("${id}")'>삭제</button>`;
+  complt.innerHTML = `<span class='item'>${content[0].innerHTML}</span>
+  <span class='item'>${content[1].innerHTML}</span>
+  <button type='button' class='remove' onClick='completeRemove("${id}")'>삭제</button>`;
 
-  complete_list.appendChild(complt);
+  completeList.appendChild(complt);
 };
 
 const todoRemove = (id) => {
   let li = document.getElementById("li" + id);
-  todo_list.removeChild(li);
+  todoList.removeChild(li);
 };
 
 const completeRemove = (id) => {
   let li = document.getElementById("li" + id);
-  complete_list.removeChild(li);
+  completeList.removeChild(li);
 };
 
-add_button.addEventListener("click", clickButton);
+addButton.addEventListener("click", clickButton);
